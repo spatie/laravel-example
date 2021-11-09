e1e9d599e48bb91db1fb76f4ce2b4c9548e2466a
freekmurze <freekmurze@users.noreply.github.com>
2021-11-09 10:11:28 +0000

    Fix styling

--- tests/Feature/MyModelTest.php
+++ tests/Feature/MyModelTest.php
@@ -2,9 +2,9 @@
 
 use Spatie\Example\Models\MyModel;
 
-it('can return the uppercased value of a name', function() {
+it('can return the uppercased value of a name', function () {
     /** @var MyModel $myModel */
-   $myModel = MyModel::factory()->create(['name' => 'John']);
+    $myModel = MyModel::factory()->create(['name' => 'John']);
 
-   expect($myModel->getUppercasedName())->toEqual('JOHN');
+    expect($myModel->getUppercasedName())->toEqual('JOHN');
 });
